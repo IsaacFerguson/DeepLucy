62858eaf93de2f1e0e026e6e4687f9979d425173
Isaac Ferguson <tul24458@temple.edu>
2023-06-29 15:49:59 -0400

    working movement and gravity

--- GolfGame/Assets/Scripts/PlayerController.cs
+++ GolfGame/Assets/Scripts/PlayerController.cs
@@ -5,36 +5,49 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {   //public float horizontal;
     //public float vertical;
-    public float speed = 6f;
-    public float sens = 100.0f;
-    public float SmoothAngle = 0.3f;
-    float turnSmoothVel;
-    public Transform cam;
+    public float speed = 12f;
+    public float gravity = -9.81f;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
     public CharacterController controller;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        //Physics.gravity = new Vector3(0, -1.0F, 0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+         isGrounded = controller.isGrounded;
 
-        if(direction.magnitude >= 0.1f){
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, SmoothAngle);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
         }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+        
+        
 
     }
 }
